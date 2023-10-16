d4fb2b59a6d5565e8ca98b4e07e1aca286ac200d
leemingoo <dwmapi@naver.com>
2023-10-16 15:02:07 +0900

    서버 확인

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const http = require('http');
 const fs = require('fs');
+const port = 8080
 
 const server = http.createServer((req, res) => {});
+
+server.listen(port, () => {
+  console.log(`http://localhost:${port}/`);
+});
