16e17540249bc9128ccca2ed4b0e9f841d2ae957
leemingoo <dwmapi@naver.com>
2023-10-16 15:04:29 +0900

    서버 에러를 익명 함수 처리 (serverErrorLog)

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const http = require('http');
 const fs = require('fs');
-const port = 8080
+const port = 8080;
 
-const server = http.createServer((req, res) => {});
+serverErrorLog = () => {
+  response.writeHead(500);
+  return response.end('서버 에러');
+}
+
+const server = http.createServer((request, response) => {
+  
+});
 
 server.listen(port, () => {
   console.log(`http://localhost:${port}/`);
